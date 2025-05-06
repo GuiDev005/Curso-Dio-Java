096ae2484e12228bc904b2641e03538e9d3ff4ea
GuiDev005 <guilhermeeduardo88130@gmail.com>
2025-05-06 20:07:35 -0300

    Atividade de java

---
+++ ContaTerminal.java
@@ -0,0 +1,30 @@
+package Contabanco;
+
+import java.util.Scanner;
+
+public class ContaTerminal{
+    public static void main(String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // consumir a quebra de linha
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo inicial:");
+        double saldo = scanner.nextDouble();
+
+        // Mensagem final formatada
+        System.out.println("\nOlá " + nomeCliente + 
+            ", obrigado por criar uma conta em nosso banco!");
+        System.out.println("Sua agência é " + agencia + 
+            ", conta " + numero + " e seu saldo de R$" + String.format("%.2f", saldo) +
+            " já está disponível para saque.");
+
+        scanner.close();
+    }
+}
